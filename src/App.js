328400e1e13f77f034a0a0bdb57fa71149be3ec8
Robin SU <48374785+rudongsu@users.noreply.github.com>
2021-01-27 11:42:27 +1030

    add delete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import CurrentLocation from "./Map";
 import * as axios from "axios";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FaEdit } from "react-icons/fa";
+import {RiDeleteBin5Line} from "react-icons/ri";
+
 import {
   Container,
   Row,
@@ -20,20 +23,20 @@ export class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: false,
-      notes: [],
-      overlay: true, 
-      showHide: false,
+      show: false, //shows maps
+      notes: [], //object to save all returned notes from DB
+      overlay: true, //loading screen overlay
       showingInfoWindow: false, // Hides or shows the InfoWindow
       activeMarker: {}, // Shows the active marker upon click
       selectedPlace: {}, // Shows the InfoWindow to the selected place upon a marker
       latValue: "", //coordinates value
       lngValue: "", //coordinates value
-      location: "",
-      usernameSaved: "",
+      location: "", //location as string
+      usernameSaved: "", //entered username
     };
     this.createNote = this.createNote.bind(this);
     this.getNote = this.getNote.bind(this);
+    this.updateNote = this.updateNote.bind(this);
   }
 
   callbackFunction = (childData) => {
@@ -65,16 +68,69 @@ export class MapContainer extends Component {
       latValue: props.mapCenter.lat,
       lngValue: props.mapCenter.lng,
     });
-
+    this.getLocation();
     this.getNote();
   };
 
+  getLocation = () => {
+    const geoLocationAPI =
+    "https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=" +
+    this.state.latValue.toString() +
+    "%2C" +
+    this.state.lngValue.toString() +
+    "%2C250&mode=retrieveAddresses&maxresults=1&gen=9&apiKey=" +
+    process.env.REACT_APP_HEREAPIKEY;
+    axios.get(geoLocationAPI).then((res)=>{
+      const reverseGeo =
+      res.data.Response.View[0].Result[0].Location.Address.Label;
+    //console.log(reverseGeo);
+    this.setState({
+      location: reverseGeo,
+    });
+    }).catch((error)=>{console.log(error);});
+  
+  }
+  
+  updateNote = (note) =>{
+
+
+  }
+
+  deleteNote = (note) =>{
+    axios.delete(noteAPI + "/" + note._id)
+    .then((res) => {
+      console.log('delete success: ', res.status);
+      this.getNote();
+    })
+    .catch((error) =>{
+      console.error('error', error);
+    })
+  }
+  
   getNote = () => {
     axios
       .get(noteAPI + "/" + this.state.usernameSaved)
       .then((res) => {
         let notes = res.data.map((note) => {
-          return <div key={note.id}>'{note.value}'</div>;
+          return (
+            <Container>
+              <Row>
+                <Col sm={8} md={8} lg={8} xs={8}>
+                  <div key={note._id}>'{note.value}'</div>
+                </Col>
+                {/* <Col sm={2} md={2} lg={2} xs={2}>
+                  <div onClick={() =>  this.updateNote(note)}>
+                  <FaEdit />
+                  </div>
+                </Col> */}
+                <Col sm={4} md={4} lg={4} xs={4}>
+                  <div onClick = {() => this.deleteNote(note)}>
+                  <RiDeleteBin5Line />
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          );
         });
         this.setState({ notes: notes });
       })
@@ -85,7 +141,8 @@ export class MapContainer extends Component {
 
   createNote = () => {
     let noteSaved = document.getElementById("notes").value;
-    let geoLocationAPI =
+
+    const geoLocationAPI =
       "https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=" +
       this.state.latValue.toString() +
       "%2C" +
@@ -98,12 +155,7 @@ export class MapContainer extends Component {
     } else {
       try {
         axios.get(geoLocationAPI).then((res) => {
-          const reverseGeo =
-            res.data.Response.View[0].Result[0].Location.Address.Label;
-          //console.log(reverseGeo);
-          this.setState({
-            location: reverseGeo,
-          });
+
           axios
             .post(noteAPI, {
               value: noteSaved,
@@ -122,13 +174,14 @@ export class MapContainer extends Component {
         alert(e);
       }
     }
-    //console.log('typeOf', typeof(this.state.latValue.toString()));
+    
   };
 
   onInfoWindowOpen = (props, e) => {
     const newWindows = (
       <Container>
-        <h3>Current Location</h3>
+        <h3><b>Current Location:</b></h3>
+        <h4><i>{this.state.location}</i></h4>
         <hr></hr>
         <InputGroup className="mb-3">
           <FormControl
@@ -195,5 +248,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_GoogleAPIKey,
+  //apiKey: process.env.REACT_APP_GoogleAPIKey,
 })(MapContainer);
